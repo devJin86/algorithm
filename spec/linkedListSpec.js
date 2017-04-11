c16f7d700df23fb237ec2fe71cd591b877a3737a
Jin Bok <devJin86@gmail.com>
2017-04-11 15:02:57 -0700

    Add basic Spec + add Tails

---
+++ spec/linkedListSpec.js
@@ -0,0 +1,27 @@
+describe('linkedList', function() {
+  var linkedList;
+
+  beforeEach(function() {
+    linkedList = LinkedList();
+  });
+
+  it('should have a head and tail', function() {
+    expect(linkedList).to.have.property('head');
+    expect(linkedList).to.have.property('tail');
+  });
+
+  it('should have methods named "addToTail", "removeHead", and "contains"', function() {
+    expect(linkedList.addToTail).to.be.a('function');
+    expect(linkedList.removeHead).to.be.a('function');
+    expect(linkedList.contains).to.be.a('function');
+  });
+
+  it('should designate a new tail when new nodes are added', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.tail.value).to.equal(4);
+    linkedList.addToTail(5);
+    expect(linkedList.tail.value).to.equal(5);
+  });
+
+
+});
